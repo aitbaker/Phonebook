1a36c17e7ae37fc5daf4e9ca196fac97044c36ac
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-05-07 11:06:56 +0200

    Update ApplicationManager.java

--- src/main/java/com/phonebook/fw/ApplicationManager.java
+++ src/main/java/com/phonebook/fw/ApplicationManager.java
@@ -13,6 +13,8 @@ public class ApplicationManager {
     String browser;
     public WebDriver driver;
 
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
+
     UserHelper user;
     ContactHelper contact;
     HomePageHelper homePage;
@@ -38,13 +40,20 @@ public class ApplicationManager {
 
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
+             logger.info("Test starts in Chrome browser");
         } else if (browser.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
+            logger.info("Test starts in Mozilla browser");
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
+             logger.info("Test starts in Edge browser");
         }
 
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+
         driver.get("https://telranedu.web.app");
+          logger.info("Current url --> " + driver.getCurrentUrl());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
