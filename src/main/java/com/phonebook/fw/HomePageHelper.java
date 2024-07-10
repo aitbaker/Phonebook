fe540cfccb498cdcb97aa149a63a745f86a0e1be
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 15:51:29 +0200

    Update HomePageHelper.java

--- src/main/java/com/phonebook/fw/HomePageHelper.java
+++ src/main/java/com/phonebook/fw/HomePageHelper.java
@@ -3,17 +3,17 @@ package com.phonebook.fw;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePageHelper extends BaseHelper{
+public class HomePageHelper extends BaseHelper {
 
     public HomePageHelper(WebDriver driver) {
         super(driver);
     }
 
     public boolean isHomeComponentPresent() {
-        return isElementPresent(By.cssSelector("div:nth-child(2)>div>div"));
+        return isElementPresent(By.cssSelector("div:nth-child(2)>div>div>h1"));
     }
 
-    public boolean isHomeComponentPresent1() {
-        return isElementPresent1(By.xpath("//h1[.='Home Component']"));
+    public void clickOnHomeLink() {
+        click(By.cssSelector("[href='/home']"));
     }
 }
