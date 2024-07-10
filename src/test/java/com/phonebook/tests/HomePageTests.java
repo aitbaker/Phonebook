fe0ea87928f39fcff76363cc031f78d754521d7a
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 15:46:57 +0200

    Update HomePageTests.java

--- src/test/java/com/phonebook/tests/HomePageTests.java
+++ src/test/java/com/phonebook/tests/HomePageTests.java
@@ -1,15 +1,22 @@
 package com.phonebook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class HomePageTests extends TestBase{
 
-    @Test
-    public void openHomePageTest() {
-        // isHomeComponentPresent();
-        //System.out.println("Home Component is " + isHomeComponentPresent1());
-        Assert.assertTrue(app.getHomePage().isHomeComponentPresent());
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!app.getHomePage().isHomeComponentPresent()) {
+            app.getHomePage().clickOnHomeLink();
+        }
     }
 
+    @Test
+    public void isHomeComponentPresentTest() {
+        Assert.assertTrue(app.getHomePage().isHomeComponentPresent());
+       // System.out.println("Home Component is " + isHomeComponentPresent());
+        //      //div[2]/div/div/h1
+    }
 }
