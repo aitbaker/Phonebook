5e65d7363279f4e436a9cc013bf3fb64691a8275
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 15:50:44 +0200

    Update ApplicationManager.java

--- src/main/java/com/phonebook/fw/ApplicationManager.java
+++ src/main/java/com/phonebook/fw/ApplicationManager.java
@@ -1,17 +1,21 @@
 package com.phonebook.fw;
 
-import org.openqa.selenium.*;
+import com.phonebook.utils.MyListener;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 public class ApplicationManager {
 
     String browser;
-    public WebDriver driver;
+    WebDriver driver;
 
     Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
 
@@ -21,6 +25,35 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+    }
+
+
+    public void init() {
+
+        System.err.close();
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
+
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+
+        //gradle clean <task_name> --> Chrome
+        //gradle -Pbrowser=firefox clean <task_name> --> Mozilla
+        //gradle -Pbrowser=edge clean <task_name> --> Mozilla
+
+        driver.get("https://telranedu.web.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        user = new UserHelper(driver);
+        contact = new ContactHelper(driver);
+        homePage = new HomePageHelper(driver);
     }
 
     public UserHelper getUser() {
@@ -33,33 +66,6 @@ public class ApplicationManager {
 
     public HomePageHelper getHomePage() {
         return homePage;
-    }
-
-    public void init() {
-        System.err.close();
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
-             logger.info("Test starts in Chrome browser");
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver();
-            logger.info("Test starts in Mozilla browser");
-        } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
-             logger.info("Test starts in Edge browser");
-        }
-
-        WebDriverListener listener = new MyListener();
-        driver = new EventFiringDecorator<>(listener).decorate(driver);
-
-        driver.get("https://telranedu.web.app");
-          logger.info("Current url --> " + driver.getCurrentUrl());
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        user = new UserHelper(driver);
-        contact = new ContactHelper(driver);
-        homePage = new HomePageHelper(driver);
     }
 
     public void stop() {
